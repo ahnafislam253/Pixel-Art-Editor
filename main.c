11959973826bdaf8da3ede142972f27f955370e3
ahnafislam253 <ahnaf.islam.253@northsouth.edu>
2025-10-21 22:43:53 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+#define WIDTH 30
+#define HEIGHT 20 
+
+char canvas [HEIGHT] [WIDTH];
+
+void initCanvas() 
+{
+  for(int i=0; i<HEIGHT; i++)
+    for (int j=0; j<WIDTH; j++)
+      canvas [i] [j] = '.';
+}
+
+void displayCanvas()
+{
+  system("cls");
+printf("\n____Pixel Art Editor____\n");
+for(int i=0; i<HEIGHT; i++)
+{ 
+  for(int j=0; j<WIDTH; j++)
+{
+  printf ("%c", canvas [i] [j]);
+}
+  printf("\n");
+}
+  printf("\n");
+}
+
+int main()
+{
+  initCanvas();
+  displayCanvas();
+return 0;
+}
