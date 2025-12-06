1124a0055b22e1627baec42fbfacb6e82ce6e6c8
ahnafislam253 <ahnaf.islam.253@northsouth.edu>
2025-12-06 21:35:20 +0600

    Refactor canvas handling and display logic
    Refactor canvas initialization and display functions to use dynamic memory allocation for canvas. Update the display function to show canvas size and improve pixel drawing functionality.

--- main.c
+++ main.c
@@ -1,48 +1,58 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define WIDTH 30
-#define HEIGHT 20 
-
-char canvas [HEIGHT] [WIDTH];
+int WIDTH, HEIGHT;
+char **canvas;
 
 void initCanvas() 
 {
-  for(int i=0; i<HEIGHT; i++)
-    for (int j=0; j<WIDTH; j++)
-      canvas [i] [j] = '.';
+    canvas = (char **)malloc(HEIGHT * sizeof(char *));
+    for (int i = 0; i < HEIGHT; i++) 
+    {
+        canvas[i] = (char *)malloc(WIDTH * sizeof(char));
+        for (int j = 0; j < WIDTH; j++) 
+        {
+            canvas[i][j] = '.';
+        }
+    }
 }
 
-void displayCanvas()
+void displayCanvas() 
 {
-  system("cls");
-printf("\n____Pixel Art Editor____\n");
-for(int i=0; i<HEIGHT; i++)
-{ 
-  for(int j=0; j<WIDTH; j++)
-{
-  printf ("%c", canvas [i] [j]);
-}
-  printf("\n");
-}
-  printf("\n");
+    system("cls");
+    printf("\n--- Pixel Art Editor ---\n");
+    printf("Canvas Size: %d x %d\n\n", HEIGHT, WIDTH);
+
+    printf("   ");
+    for (int j = 0; j < WIDTH; j++) 
+    {
+        printf("%2d", j % 10);
+    }
+    printf("\n");
+
+    for (int i = 0; i < HEIGHT; i++) 
+    {
+        printf("%2d ", i);
+        for (int j = 0; j < WIDTH; j++) 
+        {
+            printf("%c ", canvas[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 void drawPixel( int x,int y,char color )
 {
-
-
     if ( x >= 0 && x < HEIGHT&& y>= 0 && y < WIDTH )
     canvas[x][y]=color;
     else
         printf("Coordinates are invalid...\n");
-
-
 }
 
     void erasePixel(int x, int y)
 {
-        if (x>= 0&&  x< HEIGHT&& y>= 0 && y< WIDTH)
+        if (x>= 0 &&  x< HEIGHT&& y>= 0 && y< WIDTH)
 
     canvas[x][y] ='.';
         else
@@ -55,7 +65,6 @@ void fillArea( int x, int y, char target, char replacement )
         if(canvas[x][y] != target ) return;
         if(canvas[x] [y] == replacement) return;
 
-
 canvas[x][y] = replacement ;
 fillArea( x+1, y, target, replacement);
 fillArea(x-1, y, target, replacement);
@@ -63,9 +72,4 @@ fillArea (x , y+1, target, replacement);
 fillArea(x , y-1, target, replacement);
 }
 
-int main()
-{
-  initCanvas();
-  displayCanvas();
-return 0;
-}
+
